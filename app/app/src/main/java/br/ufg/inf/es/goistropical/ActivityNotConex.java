f7fb893da2172cb0d724ed93b05bb95e1f053edc
Antonio Silva <antoni-s@outlook.com>
2018-06-26 20:32:38 -0300

    Update

---
+++ app/app/src/main/java/br/ufg/inf/es/goistropical/ActivityNotConex.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.es.goistropical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityNotConex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_conex);
+    }
+}
