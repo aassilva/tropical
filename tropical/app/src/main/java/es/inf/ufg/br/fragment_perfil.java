d4930a1f98fcaf8e552b8fbd5efb8f44f240baf4
Antonio Silva <antoni-s@outlook.com>
2018-07-04 21:51:00 -0300

    algumas telas e classes javas

---
+++ tropical/app/src/main/java/es/inf/ufg/br/fragment_perfil.java
@@ -0,0 +1,13 @@
+package es.inf.ufg.br;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fragment_perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_perfil);
+    }
+}
